b6b69de4f1027dcc9af89b49238720bf814b4706
creawww <creawww@gmail.com>
2022-05-29 18:11:40 +0200

    add rpi monitos and watchtower

--- start_page/menu.js
+++ start_page/menu.js
@@ -3,42 +3,42 @@ const elements = [
     {
         name: "RPI-MONITOR",
         description: "Monitorizacion Rapsberry",
-        url: "http://192.168.1.100:8888/",
+        port_app: ":8888/",
         color: "#b81770",
         logo: "images/rpi-monitor.png"
     },
     {
         name: "FILEBROWSER",
         description: "Explorador archivos",
-        url: "http://192.168.1.100:8088/",
+        port_app: ":8088/",
         color: "#17a2b8",
         logo: "images/filebrowser.png"
     },
     {
         name: "NAVIDRONE",
         description: "Servidor de musica",
-        url: "http://192.168.1.100:4533/",
+        port_app: ":4533/",
         color: "#07afff",
         logo: "images/navidrone.png"
     },
     {
         name: "AUDIOBOOKS",
         description: "Libreria de audiolibros",
-        url: "http://192.168.1.100:13378/",
+        port_app: ":13378/",
         color: "#ff6e07",
         logo: "images/audiobooks.png"
     },
     {
         name: "TEXTO A MP3",
         description: "Pasar texto a mp3",
-        url: "http://192.168.1.100:5010/",
+        port_app: ":5010/",
         color: "#74a77f",
         logo: "images/textmp3.png",
     },
     {
         name: "AUDIO DE YOUTUBE",
         description: "Descarga Youtube en audio",
-        url: "http://192.168.1.100:5020",
+        port_app: ":5020",
         color: "#a31414",
         logo: "images/audioyoutube.png"
     },
